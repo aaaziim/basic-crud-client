86b2e2f87bf9c58c87b53bf17ea29f03e00b3f30
Azim <azim210215@gmail.com>
2025-01-01 07:57:57 -0800

    User Created From From and saved in databse

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  
+
+  const handleAddUser = (e) =>{
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newUser = {
+      name,
+      email
+    }
+
+    fetch("http://localhost:5000/addusers", {
+      method:"POST",
+      headers:{'content-type':'application/json'},
+      body: JSON.stringify(newUser)
+    })
+    .then(res=>res.json())
+    .then(data => {
+      if(data.acknowledged){
+        alert("User Added")
+      }
+    })
+
+  }
 
   return (
     <>
-        <h1>Hello</h1>
+        <h1>Simple CRUD</h1>
+
+        <form className='bg-slate-400 p-4 rounded-2xl' onSubmit={handleAddUser} >
+
+            <input className='border my-2 rounded-lg' type="text" name='name' />
+            <br />
+            <input className='border my-2 rounded-lg' type="email" name='email' />
+            <br />
+
+            <input className='btn' type="submit" value="Add User" />
+
+
+        </form>
     </>
   )
 }
